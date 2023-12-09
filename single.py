42f4b037c867a2f50c41c1d53846673382a71960
shimat <schimatk@gmail.com>
2023-12-09 17:35:20 +0900

    single

---
+++ single.py
@@ -0,0 +1,40 @@
+from collections.abc import Iterable
+from typing import TypeVar
+import more_itertools
+
+
+T = TypeVar('T')
+
+
+def single(iterable: Iterable[T]) -> T:
+    it = iter(iterable)
+    result = next(it)
+    try:
+        next(it)
+    except StopIteration:
+        return result
+    raise ValueError("Iterable has more than one item")
+
+
+# single
+data1 = [1, 2, 3,]
+data2 = []
+data3 = [1]
+
+#print(more_itertools.one(data1))
+#print(more_itertools.one(data2))
+#print(more_itertools.one(data3))
+#print(single(data1))
+#print(single(data2))
+print(single(data3))
+
+# single_or_Ddfault
+#data1 = [1, 2, 3,]
+#data2 = []
+#data3 = [1]
+
+#print(more_itertools.only(data1))
+#print(more_itertools.only(data2))
+#print(more_itertools.one(data3))
+
+
